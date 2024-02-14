20934107b324921f5ede572f0a9d2f31acbdb618
Myst3rYan <lanfyp@gmail.com>
2024-02-14 08:38:24 +0100

    MyPosition

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Share } from "react-native";
+import React, { useEffect } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    text: {
+      marginTop: 100,
+      fontSize: 20,
+      marginBottom: 100,
+    },
+  });
+
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  let latitude = null;
+  let longitude = null;
+  let altitude = null;
+
+  async function getUserLocation() {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+    let locationCurrent = await Location.getCurrentPositionAsync({});
+    setLocation(locationCurrent);
+  }
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  async function sharedPositon() {
+    try {
+      await Share.share({
+        message:
+          `Au secour ! Voici ma position.` +
+          `\nLatitude: ${latitude}, Longitude: ${longitude}, Altitude: ${altitude}` +
+          `\nhttps://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  let text = "Click to share your position";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    latitude = location.coords.latitude;
+    longitude = location.coords.longitude;
+    altitude = location.coords.altitude;
+    text =
+      `Latitude: ${latitude}` +
+      `\n Longitude: ${longitude}` +
+      `\n Altitude: ${altitude}`;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Share my position" onPress={sharedPositon} />
+      <Text style={styles.text}>{text}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
